433e809ed9dd59096efa5aaf25c6cfcc19be756e
Aaron Sulwer <asulwer@gmail.com>
2024-11-13 11:53:27 -0700

    already included in main branch committed by another user

--- src/RulesEngine/Models/ImplicitObject.cs
+++ src/RulesEngine/Models/ImplicitObject.cs
@@ -8,53 +8,32 @@ namespace RulesEngine.Models
     [ExcludeFromCodeCoverage]
     public class ImplicitObject
     {
+
         #region Implicit Operators
 
-        public static implicit operator ImplicitObject(bool value)
-        {
-            return default;
-        }
+        public static implicit operator ImplicitObject(bool value) => default;
 
-        public static implicit operator ImplicitObject(bool? value)
-        {
-            return default;
-        }
+        public static implicit operator ImplicitObject(bool? value) => default;
 
-        public static implicit operator ImplicitObject(char value)
-        {
-            return default;
-        }
+        public static implicit operator ImplicitObject(char value) => default;
 
-        public static implicit operator ImplicitObject(char? value)
-        {
-            return default;
-        }
+        public static implicit operator ImplicitObject(char? value) => default;
 
-        public static implicit operator ImplicitObject(string value)
-        {
-            return default;
-        }
+        public static implicit operator ImplicitObject(string value) => default;
 
-        public static implicit operator ImplicitObject(int value)
-        {
-            return default;
-        }
+        public static implicit operator ImplicitObject(int value) => default;
 
-        public static implicit operator ImplicitObject(int? value)
-        {
-            return default;
-        }
+        public static implicit operator ImplicitObject(int? value) => default;
 
-        public static implicit operator ImplicitObject(decimal value)
-        {
-            return default;
-        }
+        public static implicit operator ImplicitObject(decimal value) => default;
 
-        public static implicit operator ImplicitObject(decimal? value)
-        {
-            return default;
-        }
+        public static implicit operator ImplicitObject(decimal? value) => default;
+
+        public static implicit operator ImplicitObject(float value) => default;
+
+        public static implicit operator ImplicitObject(float? value) => default;
 
         #endregion
+
     }
 }
