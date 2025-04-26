4cabdd3e2180f8839450d2686f3fa2295aa70775
Renan Pereira <rpereira@glasslewis.com>
2025-04-26 11:11:16 +0100

    fix workflow scope parameter

--- src/RulesEngine/Extensions/EnumerableExtensions.cs
+++ src/RulesEngine/Extensions/EnumerableExtensions.cs
@@ -16,6 +16,8 @@ namespace RulesEngine.Extensions
 
         public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source,Func<TSource, TKey> keySelector)
         {
+            
+
             var seenKeys = new HashSet<TKey>();
 
             foreach (var element in source)
